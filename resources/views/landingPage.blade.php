cc4964bb357d921bc4aa1b2b0fd1d6c150c5b1c9
laravel-webprofil <mawoloti@gmail.com>
2024-10-23 13:29:35 +0700

    update icon in landing page

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/logo pendarasa.jpg') }}">
     <title>Pendar rasa</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap');
